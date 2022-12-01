aaeb48f1a18c0c760e79fe2e33a2cc75ebd156d5
Adiro <adiro.rostek@gmail.com>
2022-12-01 12:59:48 +0100

    Added part2

--- aoc1/aoc1.py
+++ aoc1/aoc1.py
@@ -7,5 +7,16 @@ def part1():
     print(sum(list(max([tuple(map(int, x)) for x in elves_calories], key=lambda x: sum(x)))))
 
 
+def part2():
+    with open("input.txt") as f:
+        elves_calories = f.read().split("\n\n")
+
+    elves_calories = [elve_calories.split("\n") for elve_calories in elves_calories]
+    elves_calories[-1].pop()  # remove last blank string
+    elves_calories = [tuple(map(int, x)) for x in elves_calories]
+    print(sum([sum(x) for x in sorted(elves_calories, key=lambda x: sum(x))[-3:]]))
+
+
 if __name__ == "__main__":
     part1()
+    part2()
