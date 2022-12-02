87a2f132b67c8d12dad119b6a9420e9c846b227d
xAdiro <adiro.rostek@gmail.com>
2022-12-02 15:44:09 +0100

    day 2 part 2

--- aoc2/aoc2.py
+++ aoc2/aoc2.py
@@ -11,13 +11,25 @@ VALUES = {
 BEATS = {
     "X": "C",
     "Y": "A",
-    "Z": "B"
+    "Z": "B",
+    "A": "Z",
+    "B": "X",
+    "C": "Y"
+}
+
+LOSES = {
+    "A": "Y",
+    "B": "Z",
+    "C": "X"
 }
 
 DRAWS = {
     "X": "A",
     "Y": "B",
-    "Z": "C"
+    "Z": "C",
+    "A": "X",
+    "B": "Y",
+    "C": "Z"
 }
 
 
@@ -41,5 +53,32 @@ def part1():
     print(score)
 
 
+def part2():
+    with open("input.txt") as f:
+        moves = f.read().splitlines()
+
+    score = 0
+
+    for move in moves:
+        their_move = move[0]
+        result = move[2]
+        my_move = ""
+
+        match result:
+            case "X":
+                my_move = BEATS[their_move]
+            case "Y":
+                my_move = DRAWS[their_move]
+                score += 3
+            case "Z":
+                my_move = LOSES[their_move]
+                score += 6
+
+        score += VALUES[my_move]
+
+    print(score)
+
+
 if __name__ == "__main__":
     part1()
+    part2()
